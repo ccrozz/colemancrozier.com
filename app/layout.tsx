5c15623d9d15b8577bf9366303e732248ba53413
ccrozz <crozier.coleman@gmail.com>
2026-06-08 16:33:43 -0400

    analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/next";
 import {
   Cormorant_Garamond,
   DM_Sans,
@@ -51,7 +52,10 @@ export default function RootLayout({
       lang="en"
       className={`${display.variable} ${heading.variable} ${body.variable} ${mono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
